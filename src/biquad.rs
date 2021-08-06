b412ddcfc7717b187f84fb5ff97f901e957fd4cb
Joe Neeman <joeneeman@gmail.com>
2021-08-06 09:30:04 -0500

    Start on the training code.

--- src/biquad.rs
+++ src/biquad.rs
@@ -1,9 +1,10 @@
-pub(crate) const BIQUAD_HP: Biquad = Biquad {
+pub const BIQUAD_HP: Biquad = Biquad {
     a: [-1.99599, 0.99600],
     b: [-2.0, 1.0],
 };
 
-pub(crate) struct Biquad {
+#[derive(Default)]
+pub struct Biquad {
     pub a: [f32; 2],
     pub b: [f32; 2],
 }
@@ -20,6 +21,20 @@ impl Biquad {
             mem[0] = (mem[1] as f64 + (b0 * x64 - a0 * y64)) as f32;
             mem[1] = (b1 * x64 - a1 * y64) as f32;
             *y = y64 as f32;
+        }
+    }
+
+    pub fn filter_in_place(&self, data: &mut [f32], mem: &mut [f32; 2]) {
+        let a0 = self.a[0] as f64;
+        let a1 = self.a[1] as f64;
+        let b0 = self.b[0] as f64;
+        let b1 = self.b[1] as f64;
+        for x in data {
+            let x64 = *x as f64;
+            let y64 = x64 + mem[0] as f64;
+            mem[0] = (mem[1] as f64 + (b0 * x64 - a0 * y64)) as f32;
+            mem[1] = (b1 * x64 - a1 * y64) as f32;
+            *x = y64 as f32;
         }
     }
 }
