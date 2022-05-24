82f3abcda0da680cb4cec7c36147c9059d618a3f
Joe Neeman <joeneeman@gmail.com>
2022-05-24 14:34:57 -0500

    Add some basic CLI tests.

---
+++ tests/cli.rs
@@ -0,0 +1,42 @@
+use assert_cmd::prelude::*;
+use assert_fs::prelude::*;
+use predicates::prelude::*;
+use std::process::Command;
+
+#[test]
+fn basic_usage() -> anyhow::Result<()> {
+    let mut cmd = Command::cargo_bin("nnnoiseless")?;
+    let tmp = assert_fs::TempDir::new()?;
+    let input = tmp.child("input.raw");
+    let output = tmp.child("output.raw");
+    input.write_binary(&vec![0u8; 480 * 10])?;
+
+    cmd.arg(input.path()).arg(output.path());
+    cmd.assert().success();
+    assert!(output.exists());
+    Ok(())
+}
+
+#[test]
+fn invalid_wav() -> anyhow::Result<()> {
+    let mut cmd = Command::cargo_bin("nnnoiseless")?;
+    let tmp = assert_fs::TempDir::new()?;
+    let input = tmp.child("input.wav");
+    let output = tmp.child("output.wav");
+    input.write_binary(&vec![0u8; 480 * 10])?;
+
+    cmd.arg(input.path()).arg(output.path());
+    cmd.assert()
+        .failure()
+        .stderr(predicates::str::contains("no RIFF tag found"));
+
+    let input = tmp.child("input.raw");
+    input.write_binary(&vec![0u8; 480 * 10])?;
+    let mut cmd = Command::cargo_bin("nnnoiseless")?;
+    cmd.arg("--wav-in").arg(input.path()).arg(output.path());
+    cmd.assert()
+        .failure()
+        .stderr(predicates::str::contains("no RIFF tag found"));
+
+    Ok(())
+}
